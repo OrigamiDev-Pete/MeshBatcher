b6bb7ace31d3d84534509ef74f38fed0b471d0a3
Peter-Laptop <greenmoggle@msn.com>
2020-05-23 21:27:46 +1000

    Added Unbatch function

--- addons/MultiMeshBatcher/Batcher_Node.gd
+++ addons/MultiMeshBatcher/Batcher_Node.gd
@@ -1,8 +1,20 @@
 tool
 extends Spatial
 
+var _batchedmmi : MultiMeshInstance = null
+
+func batch_on_run():
+	# Automatically batch meshes if they are not already batched for improved performance
+	# while running the scene
+	pass
+
 
 func batch():
+	#Check if already baked
+	if _batchedmmi:
+		print("Unbatch before rebatching.")
+		return
+
 	var meshes := get_children()
 	var mesh_transforms : Array = []
 	for i in meshes:
@@ -11,33 +23,52 @@ func batch():
 		else:
 			# Create a popup
 			print("Only MeshInstances can be batched.")
-		
+			return
+	
+	for i in meshes:
 		mesh_transforms.append(i.transform)
 		i.queue_free()
 	
 	var mm = MultiMesh.new()
 	var mmi = MultiMeshInstance.new()
-	mmi.name = "BatchedMesh"
+	mmi.name = "BatchedMultiMesh"
 	add_child(mmi)
 	mmi.set_owner(get_tree().get_edited_scene_root())
 	
 	mmi.multimesh = mm
-	mm.mesh = meshes[0].mesh
+	mm.mesh = meshes[0].mesh # TODO: Throw error if meshes are not the same
 	mm.transform_format = MultiMesh.TRANSFORM_3D
 	mm.instance_count = mesh_transforms.size()
 	
+	# Set transforms to each mesh instance
 	for i in mm.instance_count:
 		mm.set_instance_transform(i, mesh_transforms[i])
+	_batchedmmi = mmi
 
 
 func unbatch():
-	pass
+	if _batchedmmi == null:
+		print("Nothing to unbatch.")
+		return
+	
+	var mm : MultiMesh = _batchedmmi.multimesh
+	
+	# Get transforms from multimesh and set to each MeshInstance
+	for i in mm.instance_count:
+		var mesh := MeshInstance.new()
+		mesh.mesh = mm.mesh
+		mesh.transform = mm.get_instance_transform(i)
+		
+		add_child(mesh)
+		mesh.set_owner(get_tree().get_edited_scene_root())
+		
+	_batchedmmi.queue_free()
+	_batchedmmi = null
 
 
 func _get_configuration_warning() -> String:
 	var children := get_children()
 	for i in children.size():
-		print(children[i])
 		if children[i] is MeshInstance:
 			continue
 		if children[i] is MultiMeshInstance:
